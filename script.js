f0e70d2386b471b910fb97b68f588131c8d766b7
gcamaci <77690094+gcamaci@users.noreply.github.com>
2022-06-18 21:59:41 -0500

    need to add rows

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-const Production = (name, masterEvent, date, venue) => {
-    return{ name,masterEvent,date,venue}
+const Production = (name, masterEvent, date,time, venue) => {
+    return{ name,masterEvent,date,time,venue}
 };
 
-
-let eventArray = [];
-
-const wb = XLSX.utils.book_new();
-wb.SheetNames.push("Test Sheet");
-let ws_data = []
-
-
 const displayController = (() => {
+    let eventArray = [];
     let dates = [];
     let counter = 0;
     let lastInput;
     const newDate = document.createElement('div');
     const create = document.getElementById('create');
+    const newBook = document.getElementById('new-book');
     newDate.classList.add('newDate');
-
-    
 
 
     const makeDate = (a) => {
@@ -43,6 +35,32 @@ const displayController = (() => {
         }
         
     };
+    const formatDate = (dte) => {
+        const dateSplit = dte.split('-');
+        const day = [dateSplit[1],dateSplit[2],dateSplit[0]].join('/')
+        return day
+
+    };
+    const changeTime = time => {
+        time = time.split(':'); // convert to array
+        let hours = Number(time[0]);
+        let minutes = Number(time[1]);
+
+      // calculate
+        let timeValue;
+
+        if (hours > 0 && hours <= 12) {
+          timeValue= "" + hours;
+        } else if (hours > 12) {
+          timeValue= "" + (hours - 12);
+        } else if (hours == 0) {
+          timeValue= "12";
+        }
+        timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes;  // get minutes
+        // get seconds
+        timeValue += (hours >= 12) ? " PM" : " AM";  // get AM/PM
+        return timeValue
+      };
 
     const createEvent = () => {
         const allDates = document.querySelectorAll('.date');
@@ -50,7 +68,11 @@ const displayController = (() => {
         const masterEvent = document.getElementById('master-event');
         const venue = document.getElementById('venue');
         allDates.forEach((date)=>{
-            const newEvent = Production(name.value,masterEvent.value,date.value,venue.value);
+            const splitDate = date.value.split(' ');
+            const day = formatDate(splitDate[0])
+            const dateTime = changeTime(splitDate[1])
+            
+            const newEvent = Production(name.value,masterEvent.value,day,dateTime,venue.value);
             eventArray.push(newEvent);
             date.value = ''
             //console.log(date.value);
@@ -64,16 +86,12 @@ const displayController = (() => {
 
     create.addEventListener('click',()=>{
         createEvent()
-        eventArray.forEach((per)=>{
-            let rowArray = [per.name,per.masterEvent,per.venue,per.date]
-            ws_data.push(rowArray);
-        });
-        
-        const ws = XLSX.utils.aoa_to_sheet(ws_data);
-        wb.Sheets["Test Sheet"] = ws;
-            
+        console.log(eventArray)
     
-        
+    });
+
+    newBook.addEventListener('click', () => {
+        createBook()
     });
 
     const buttons = document.querySelectorAll('.btn');
@@ -85,51 +103,36 @@ const displayController = (() => {
     }); 
 
     
-    return {makeDate,dates}
+    return {makeDate,dates,eventArray}
 })();
 
 
-const submit = document.querySelector('#submit');
+function createBook() {
+    const submit = document.querySelector('#submit');
+    const wb = XLSX.utils.book_new();
+    wb.SheetNames.push("Test Sheet");
+    let ws_data = []
+    displayController.eventArray.forEach((per)=>{
+        let rowArray = [per.name,per.masterEvent,per.venue,per.date]
+        ws_data.push(rowArray);
+    });
 
+    const ws = XLSX.utils.aoa_to_sheet(ws_data);
+    wb.Sheets["Test Sheet"] = ws;
 
+    const wbout = XLSX.write(wb,{bookType: 'xlsx', type:'binary'});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-eventArray.forEach((per)=>{
-    let rowArray = [per.name,per.masterEvent,per.venue,per.date]
-    ws_data.push(rowArray);
-});
-
-const ws = XLSX.utils.aoa_to_sheet(ws_data);
-wb.Sheets["Test Sheet"] = ws;
-*/
+    function s2ab(s) {
+        var buf = new ArrayBuffer(s.length);
+        var view = new Uint8Array(buf);
+        for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
+        return buf;
     
+    }
 
-const wbout = XLSX.write(wb,{bookType: 'xlsx', type:'binary'});
+    submit.addEventListener('click',() => {
+        saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), 'test.xlsx');
 
-function s2ab(s) {
-  
-    var buf = new ArrayBuffer(s.length);
-    var view = new Uint8Array(buf);
-    for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
-    return buf;
-    
-}
-
-submit.addEventListener('click',() => {
-    saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), 'test.xlsx');
-
-});
+    });
+};
 
